9a714043e3e6306496253d92c1ab379fbe2186d4
Mashrafi Rahman <monomycelium@icloud.com>
2024-07-02 16:26:59 +0800

    Rename `Chunk` to `ChunkType`.

--- src/types.zig
+++ src/types.zig
@@ -1,25 +1,25 @@
 const std = @import("std");
 const testing = std.testing;
 
-const Chunk = struct {
-    const ChunkError = error{InvalidChunk};
+const ChunkType = struct {
+    const ChunkTypeError = error{InvalidChunkType};
 
     bytes: [4]u8,
 
-    pub fn init(bytes: [4]u8) Chunk.ChunkError!Chunk {
-        var chunk: Chunk = undefined;
+    pub fn init(bytes: [4]u8) ChunkTypeError!ChunkType {
+        var chunk_type: ChunkType = undefined;
 
         for (bytes, 0..) |byte, i| {
             if (!std.ascii.isAlphabetic(byte))
-                return error.InvalidChunk;
+                return error.InvalidChunkType;
 
-            chunk.bytes[i] = byte;
+            chunk_type.bytes[i] = byte;
         }
 
-        return chunk;
+        return chunk_type;
     }
 
-    pub fn is_valid(self: Chunk) bool {
+    pub fn is_valid(self: ChunkType) bool {
         if (!self.is_reserved_bit_valid())
             return false;
 
@@ -30,24 +30,24 @@ const Chunk = struct {
         return true;
     }
 
-    pub fn is_critical(self: Chunk) bool {
+    pub fn is_critical(self: ChunkType) bool {
         return self.bytes[0] >> 5 & 1 == 0;
     }
 
-    pub fn is_public(self: Chunk) bool {
+    pub fn is_public(self: ChunkType) bool {
         return self.bytes[1] >> 5 & 1 == 0;
     }
 
-    pub fn is_reserved_bit_valid(self: Chunk) bool {
+    pub fn is_reserved_bit_valid(self: ChunkType) bool {
         return self.bytes[2] >> 5 & 1 == 0;
     }
 
-    pub fn is_safe_to_copy(self: Chunk) bool {
+    pub fn is_safe_to_copy(self: ChunkType) bool {
         return self.bytes[3] >> 5 & 1 == 1;
     }
 
     pub fn format(
-        self: Chunk,
+        self: ChunkType,
         comptime fmt: []const u8,
         options: std.fmt.FormatOptions,
         writer: anytype,
@@ -64,77 +64,77 @@ const Chunk = struct {
     }
 };
 
-test "chunk_type_from_bytes" {
+test "chunk_type_type_from_bytes" {
     const expected = [4]u8{ 82, 117, 83, 116 };
-    const actual = try Chunk.init([4]u8{ 82, 117, 83, 116 });
+    const actual = try ChunkType.init([4]u8{ 82, 117, 83, 116 });
 
     try testing.expectEqualSlices(u8, &expected, &actual.bytes);
 }
 
-test "chunk_type_from_str" {
-    const expected = try Chunk.init([4]u8{ 82, 117, 83, 116 });
-    const actual = try Chunk.init("RuSt".*);
+test "chunk_type_type_from_str" {
+    const expected = try ChunkType.init([4]u8{ 82, 117, 83, 116 });
+    const actual = try ChunkType.init("RuSt".*);
     try testing.expectEqualSlices(u8, &expected.bytes, &actual.bytes);
 }
 
-test "chunk_type_is_critical" {
-    const chunk = try Chunk.init("RuSt".*);
-    try testing.expect(chunk.is_critical());
+test "chunk_type_type_is_critical" {
+    const chunk_type = try ChunkType.init("RuSt".*);
+    try testing.expect(chunk_type.is_critical());
 }
 
-test "chunk_type_is_not_critical" {
-    const chunk = try Chunk.init("ruSt".*);
-    try testing.expect(!chunk.is_critical());
+test "chunk_type_type_is_not_critical" {
+    const chunk_type = try ChunkType.init("ruSt".*);
+    try testing.expect(!chunk_type.is_critical());
 }
 
-test "chunk_type_is_public" {
-    const chunk = try Chunk.init("RUSt".*);
-    try testing.expect(chunk.is_public());
+test "chunk_type_type_is_public" {
+    const chunk_type = try ChunkType.init("RUSt".*);
+    try testing.expect(chunk_type.is_public());
 }
 
-test "chunk_type_is_not_public" {
-    const chunk = try Chunk.init("RuSt".*);
-    try testing.expect(!chunk.is_public());
+test "chunk_type_type_is_not_public" {
+    const chunk_type = try ChunkType.init("RuSt".*);
+    try testing.expect(!chunk_type.is_public());
 }
 
-test "chunk_type_is_reserved_bit_valid" {
-    const chunk = try Chunk.init("RuSt".*);
-    try testing.expect(chunk.is_reserved_bit_valid());
+test "chunk_type_type_is_reserved_bit_valid" {
+    const chunk_type = try ChunkType.init("RuSt".*);
+    try testing.expect(chunk_type.is_reserved_bit_valid());
 }
 
-test "chunk_type_is_reserved_bit_invalid" {
-    const chunk = try Chunk.init("Rust".*);
-    try testing.expect(!chunk.is_reserved_bit_valid());
+test "chunk_type_type_is_reserved_bit_invalid" {
+    const chunk_type = try ChunkType.init("Rust".*);
+    try testing.expect(!chunk_type.is_reserved_bit_valid());
 }
 
-test "chunk_type_is_safe_to_copy" {
-    const chunk = try Chunk.init("RuSt".*);
-    try testing.expect(chunk.is_safe_to_copy());
+test "chunk_type_type_is_safe_to_copy" {
+    const chunk_type = try ChunkType.init("RuSt".*);
+    try testing.expect(chunk_type.is_safe_to_copy());
 }
 
-test "chunk_type_is_unsafe_to_copy" {
-    const chunk = try Chunk.init("RuST".*);
-    try testing.expect(!chunk.is_safe_to_copy());
+test "chunk_type_type_is_unsafe_to_copy" {
+    const chunk_type = try ChunkType.init("RuST".*);
+    try testing.expect(!chunk_type.is_safe_to_copy());
 }
 
-test "valid_chunk_is_valid" {
-    const chunk = try Chunk.init("RuSt".*);
-    try testing.expect(chunk.is_valid());
+test "valid_chunk_type_is_valid" {
+    const chunk_type = try ChunkType.init("RuSt".*);
+    try testing.expect(chunk_type.is_valid());
 }
 
-test "invalid_chunk_is_valid" {
+test "invalid_chunk_type_is_valid" {
     {
-        const chunk: ?Chunk = Chunk.init("Rust".*) catch null;
-        try testing.expect(chunk != null and !chunk.?.is_valid());
+        const chunk_type: ?ChunkType = ChunkType.init("Rust".*) catch null;
+        try testing.expect(chunk_type != null and !chunk_type.?.is_valid());
     }
 
     {
-        const chunk: ?Chunk = Chunk.init("Ru1t".*) catch null;
-        try testing.expect(chunk == null);
+        const chunk_type: ?ChunkType = ChunkType.init("Ru1t".*) catch null;
+        try testing.expect(chunk_type == null);
     }
 }
 
-test "chunk_type_string" {
-    const chunk = try Chunk.init("RuSt".*);
-    try testing.expectEqualSlices(u8, &chunk.bytes, "RuSt");
+test "chunk_type_type_string" {
+    const chunk_type = try ChunkType.init("RuSt".*);
+    try testing.expectEqualSlices(u8, &chunk_type.bytes, "RuSt");
 }
